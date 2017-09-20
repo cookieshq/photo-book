254788e9c7a452b764e0e78818661fb21b913270
denissellu <hingasellu@gmail.com>
2017-09-20 16:43:55 +0100

    add the camera button in the nav

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Camera, CameraOptions } from '@ionic-native/camera'
 
 @Component({
   selector: 'page-home',
@@ -7,8 +8,11 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, private camera: Camera) {
 
   }
 
+  takePhoto() {
+    console.log("Hello World")
+  }
 }
