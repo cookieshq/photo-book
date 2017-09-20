8a32b3b3bcb0f7b41849d4b149f709d81b13d586
denissellu <hingasellu@gmail.com>
2017-09-20 17:46:22 +0100

    ability to take a picture with camera

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -13,6 +13,22 @@ export class HomePage {
   }
 
   takePhoto() {
-    console.log("Hello World")
+    const options: CameraOptions = {
+      quality: 100,
+      allowEdit : true,
+      targetWidth: 300,
+      targetHeight: 300,
+      destinationType: this.camera.DestinationType.FILE_URI,
+      encodingType: this.camera.EncodingType.JPEG,
+      sourceType : this.camera.PictureSourceType.CAMERA,
+      mediaType: this.camera.MediaType.PICTURE,
+      saveToPhotoAlbum: false
+    }
+
+    this.camera.getPicture(options).then((imageUri) => {
+      console.log("imageUri is "+imageUri);
+    }, (err) => {
+      console.log("camera error is"+err);
+    });
   }
 }
