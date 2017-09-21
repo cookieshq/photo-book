a83a3dd5172603586fe815b07fcbbd0075f52ca4
denissellu <hingasellu@gmail.com>
2017-09-21 14:37:55 +0100

    change to using file url as it’s better and more performant

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -11,7 +11,7 @@ export class HomePage {
 
   public images: string[] = new Array();
 
-  constructor(public navCtrl: NavController, private camera: Camera) {
+  constructor(public navCtrl: NavController, private camera: Camera, private nativeStorage: NativeStorage) {
   }
 
   takePhoto() {
@@ -20,7 +20,7 @@ export class HomePage {
       allowEdit : true,
       targetWidth: 500,
       targetHeight: 500,
-      destinationType: this.camera.DestinationType.DATA_URL,
+      destinationType: this.camera.DestinationType.FILE_URI,
       encodingType: this.camera.EncodingType.JPEG,
       sourceType : this.camera.PictureSourceType.CAMERA,
       mediaType: this.camera.MediaType.PICTURE,
@@ -31,6 +31,12 @@ export class HomePage {
       console.log("imageUri is "+imageUri);
       this.images.push("data:image/png;base64,"+ imageUri);
       let imageUris = this.images.join(',');
+
+      this.nativeStorage.setItem('photos', imageUris)
+        .then(
+          () => console.log('Stored item!'),
+          error => console.error('Error storing item', error)
+        );
     }, (err) => {
       console.log("camera error is"+err);
     });
